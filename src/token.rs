02bdf15a987a3073bcd7b4b4825f5185e1adb52f
vinhtc27 <vinhtc27@gmail.com>
2024-10-30 00:33:18 +0700

    pass 50, 51, 52, 53th stage

--- src/token.rs
+++ src/token.rs
@@ -154,7 +154,7 @@ pub enum Value {
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Value::Number(n) => write!(f, "{}", n),
+            Value::Number(n) => write!(f, "{:?}", n),
             Value::Boolean(b) => write!(f, "{}", b),
             Value::String(s) => write!(f, "{}", s),
             Value::Nil => write!(f, "nil"),
